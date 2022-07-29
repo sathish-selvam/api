af5f1a9b04337bf865dc65c22d2f3520f65c33f9
Sathish Selvam <sathishselvamdev@gmail.com>
2022-07-29 23:43:23 +0530

    added particles js

--- public/particles_init.js
+++ public/particles_init.js
@@ -1,5 +1,3 @@
-window.onload = function () {
-  Particles.init({
-    selector: ".annimation-warpper",
-  });
-};
+particlesJS.load("particles-js", "/static/particlesjs.json", function () {
+  console.log("callback - particles.js config loaded");
+});
